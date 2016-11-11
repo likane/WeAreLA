da2a5947124ca99a394080a389c4d1a2503ac877
Louis Kane <louiskane@1234.local>
2016-11-10 21:32:59 -0800

    Initial PsudoCode

--- assets/javascript/appLa.js
+++ assets/javascript/appLa.js
@@ -2,4 +2,23 @@
 
 //BACKEND SUDOCODE
 // Timer Function
-// On-Click of Search Button
+// On-Click of Search Button 
+	//Validate user in put of no special characters -LOU
+	// Move search bar, title and temperature to top of page. Use jquery animate or jquery ui
+	// Display results (5 results with image per row) under search bar div
+	// display static background image behind results (possibly beach pic?)
+	//YELP API
+		//CITY- LOS ANGELES: location.city
+		//deals
+
+	// GROUPON API
+		//location - LOS ANGELES
+		// DEALS
+	//FORCAST.IO (DARKSKY)
+		//LOS ANGELES
+			//CURRENT CONDITIONS:
+				//TEMP
+	//BACKGROUND:
+		//TIMER Function
+		//background will change based on timer. Pics of LA.
+	// clear search bar
