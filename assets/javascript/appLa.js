e62e3584f88e5bd670a5e749638a6c8da8edb728
Gino Park <gino.park86@gmail.com>
2016-11-11 09:57:27 -0800

    CHANGES TO appLa.js: created empty search arrays, created renderSearchData function, and started click event tied to search bar

--- assets/javascript/appLa.js
+++ assets/javascript/appLa.js
@@ -1,6 +1,58 @@
-function renderSearch(){
-  //empty section
-  $("#someId").empty();
+$(function() {
+    console.log( "ready!" );
+    //created empty array to hold search data for each API
+    var searchDataYelp = [];
+    var searchDataForecast = [];
+    var searchDataTripAdvisor = [];
 
-  //create 5 section
-}
+    //function to display search data onto page
+  function renderSearchData(){
+      //empty the search section before appedning new search data
+      $("#").empty();
+      //create 5 sections with for loop
+      for (var i = 0; i < searchDataYelp.length; i++) {
+        var searchDivYelp = $("");
+        searchDivYelp.addClass("");
+        searchDivYelp.attr("data-name", searchDataYelp[i]);
+        searchDivYelp.text(searchDataYelp[i]);
+        $("#").append(searchDivYelp);
+      }
+      for (var j = 0; j < searchDataForecast.length; j++) {
+        var searchDivForecast = $("");
+        searchDivForecast.addClass("");
+        searchDivForecast.attr("data-name", searchDataForecast[j]);
+        searchDivForecast.text(searchDataForecast[j]);
+        $("#").append(searchDivForecast);
+      }
+      for (var k = 0; k < searchDataTripAdvisor.length; k++) {
+        var searchDivTripAdvisor = $("");
+        searchDivTripAdvisor.addClass("");
+        searchDivTripAdvisor.attr("data-name", searchDataTripAdvisor[k]);
+        searchDivTripAdvisor.text(searchDataTripAdvisor[k]);
+        $("#").append(searchDivTripAdvisor);
+      }
+
+    //when user clicks or hits enter in the search bar
+    $(document).on("click",/*class*/, function(){
+      //get api key
+      //get user input from search bar
+      //set queryURL
+
+      $.ajax({url:/*queryURL*/, method: "GET"})
+        .done(function(response){
+
+        });
+
+    });
+
+
+  }
+
+
+
+
+
+
+
+
+});
